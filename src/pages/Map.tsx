daf1d4e9651f4a35a0fe0b16b93ded0b727a930e
ErenElagz <erenelagoz@gmail.com>
2024-08-02 13:14:35 +0300

    chore: Update Map component with optimized route and place information display

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,16 +1,17 @@
-import { StyleSheet } from 'react-native'
+import { ScrollView, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps' // remove PROVIDER_GOOGLE import if not using Google Maps
 import { CustomMaps } from '../assets/custom-maps'
 import MapViewDirections from 'react-native-maps-directions'
-import {MAP_API_KEY} from '@env'
+import { MAP_API_KEY } from '@env'
+import { View, Text } from 'react-native'
 
 export default function Map({ route }) {
     const GOOGLE_MAPS_APIKEY = MAP_API_KEY
     const { places } = route.params
 
     const [optimizedRoute, setOptimizedRoute] = useState([])
-
+    let i=0
     useEffect(() => {
         const fetchOptimizedRoute = async () => {
             const waypoints = places.slice(1, places.length - 1).map((place) => ({
@@ -45,52 +46,82 @@ export default function Map({ route }) {
     }, [places])
 
     return (
-        <MapView
-            style={{ width: '100%', height: '100%' }}
-            customMapStyle={CustomMaps}
-            provider={PROVIDER_GOOGLE}
-            loadingEnabled={true}
-            initialRegion={{
-                latitude: 41.0082,
-                longitude: 28.9784,
-                latitudeDelta: 1,
-                longitudeDelta: 1
-            }}
-        >
-            {optimizedRoute.length > 0 &&
-                optimizedRoute.map((place, index) => (
-                    <Marker
-                        key={index}
-                        coordinate={{ latitude: place.latitude, longitude: place.longitude }}
-                        title={place.name}
-                        description={place.description}
-                    />
-                ))}
+        <>
+            <MapView
+                style={{ width: '100%', height: '100%' }}
+                customMapStyle={CustomMaps}
+                provider={PROVIDER_GOOGLE}
+                loadingEnabled={true}
+                initialRegion={{
+                    latitude: 41.0082,
+                    longitude: 28.9784,
+                    latitudeDelta: 1,
+                    longitudeDelta: 1
+                }}
+            >
+                {optimizedRoute.length > 0 &&
+                    optimizedRoute.map((place, index) => (
+                        <Marker
+                            key={index}
+                            coordinate={{ latitude: place.latitude, longitude: place.longitude }}
+                            title={place.name}
+                            description={place.description}
+                        />
+                    ))}
 
-            {optimizedRoute.length > 0 && (
-                <MapViewDirections
-                    origin={optimizedRoute[0]}
-                    destination={optimizedRoute[optimizedRoute.length - 1]}
-                    apikey={GOOGLE_MAPS_APIKEY}
-                    strokeWidth={3}
-                    waypoints={optimizedRoute.slice(1, -1)}
-                    strokeColor='darkgreen'
-                    mode='DRIVING'
-                />
-            )}
-            {places.length > 0 && (
-                <MapViewDirections
-                    origin={places[0]}
-                    destination={places[places.length - 1]}
-                    apikey={GOOGLE_MAPS_APIKEY}
-                    strokeWidth={2}
-                    waypoints={places.slice(1, -1)}
-                    strokeColor='red'
-                    mode='DRIVING'
-                />
-            )}
-        </MapView>
+                {optimizedRoute.length > 0 && (
+                    <MapViewDirections
+                        origin={optimizedRoute[0]}
+                        destination={optimizedRoute[optimizedRoute.length - 1]}
+                        apikey={GOOGLE_MAPS_APIKEY}
+                        strokeWidth={3}
+                        waypoints={optimizedRoute.slice(1, -1)}
+                        strokeColor='darkgreen'
+                        mode='DRIVING'
+                    />
+                )}
+                {places.length > 0 && (
+                    <MapViewDirections
+                        origin={places[0]}
+                        destination={places[places.length - 1]}
+                        apikey={GOOGLE_MAPS_APIKEY}
+                        strokeWidth={2}
+                        waypoints={places.slice(1, -1)}
+                        strokeColor='red'
+                        mode='DRIVING'
+                    />
+                )}
+            </MapView>
+            <ScrollView style={styles.info}>
+                <Text style={{ fontSize: 24, fontWeight: '700', color: '#000' }}>Places</Text>
+                {places.map((place, index) => (
+                    <View key={index} style={styles.card}>
+                        <Text style={{ fontSize: 14, fontWeight: '700', color: '#000' }}>{((i++) ,place.name)}</Text>
+                        <Text style={{ fontSize: 12, fontWeight: '400', color: '#aaa' }}>{place.description}</Text>
+                    </View>
+                ))}
+                <Text style={{ fontSize: 24, fontWeight: '700', color: '#000' }}>Optimized Route</Text>
+                {optimizedRoute.map((place, index) => (
+                    <View key={index} style={styles.card}>
+                        <Text style={{ fontSize: 14, fontWeight: '700', color: '#000' }}>{place.name}</Text>
+                        <Text style={{ fontSize: 12, fontWeight: '400', color: '#aaa' }}>{place.description}</Text>
+                    </View>
+                ))}
+            </ScrollView>
+        </>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    info: {
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 8,
+        position: 'absolute',
+        bottom: 16,
+        width: '90%',
+        height: '30%',
+        alignSelf: 'center',
+    },
+    card: { backgroundColor: 'white', padding: 8, borderRadius: 16, marginVertical: 8 }
+})
